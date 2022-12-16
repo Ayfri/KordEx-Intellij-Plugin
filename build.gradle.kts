11b1029b27388482cc79f173f90f6083ebe0db61
Ayfri <pierre.ayfri@gmail.com>
2022-12-16 05:16:21 +0100

    chore(kotlin): Update kotlin to 1.7.22.

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
 	java
-	kotlin("jvm") version "1.7.20"
+	kotlin("jvm") version "1.7.22"
 	id("org.jetbrains.intellij") version "1.10.1"
 }
 
