e1d12d2a925ceb599d693b3aa6beb08c2fe60f0a
Ayfri <pierre.ayfri@gmail.com>
2022-12-16 05:19:36 +0100

    fix(compatibility): Fix compatibility issue in Threading mode for actions.

--- src/main/kotlin/io/ayfri/kordexplugin/features/GoToActionAction.kt
+++ src/main/kotlin/io/ayfri/kordexplugin/features/GoToActionAction.kt
@@ -1,6 +1,7 @@
 package io.ayfri.kordexplugin.features
 
 import com.intellij.featureStatistics.FeatureUsageTracker
+import com.intellij.openapi.actionSystem.ActionUpdateThread
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.actionSystem.CommonDataKeys
@@ -16,27 +17,29 @@ class GoToActionAction : AnAction() {
 		val caret = e.getRequiredData(CommonDataKeys.CARET)
 		val psiElement = file.findElementAt(caret.offset) ?: return
 		val commandExpression = psiElement.parentOfType<KtCallExpression>() ?: return
-		
+
 		commandExpression.resolveToCall()?.let {
 			if (!it.isValidKordExExpression()) return@actionPerformed
 		} ?: return
-		
+
 		moveCursorToAction(commandExpression)
 	}
-	
+
+	override fun getActionUpdateThread() = ActionUpdateThread.BGT
+
 	override fun update(e: AnActionEvent) {
 		val file = e.getData(CommonDataKeys.PSI_FILE)
 		val caret = e.getData(CommonDataKeys.CARET)
-		
+
 		e.presentation.isEnabledAndVisible = file?.findElementAt(caret?.offset ?: return) != null
 	}
-	
+
 	companion object {
 		fun findActionElement(kordExExpression: KtCallExpression) =
 			PsiTreeUtil.findChildrenOfAnyType(kordExExpression, true, KtCallExpression::class.java).firstOrNull {
 				it.firstChild?.text == "action"
 			}
-		
+
 		fun moveCursorToAction(commandExpression: KtCallExpression) {
 			val action = findActionElement(commandExpression) ?: return
 			action.navigate(true)
